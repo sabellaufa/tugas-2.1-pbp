86b84a05ac79b6d1cd93f7ca3f26c3853bb19d79
sabellaufa <161696882+sabellaufa@users.noreply.github.com>
2024-10-04 09:53:01 +0700

    -
    -

---
+++ task4.js
@@ -0,0 +1,13 @@
+//function 
+function penjumlahan (a, b) {
+    return jumlah = a + b;
+}
+console.log(penjumlahan(1,8));
+
+//arrow function
+const input = (y,i) => y + i;
+console.log (input(1,8));
+
+//perbedaan 
+//function menggunakan kata kunci function memiliki sintaksis panjang 
+//arrow function fungsi yang sama namun lebih ringkas dan lebih banyak operator ternary
